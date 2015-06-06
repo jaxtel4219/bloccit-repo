3804b291df3317093991107b338dd4b9092a5200
jaxtel4219 <jaxtel4219@gmail.com>
2015-06-06 16:55:20 +0000

    Checkpoint-38-Associations completed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   
-  config.action_mailer.default_url_options = { host: 'http://jaxtel4219ruby-218212.use1-2.nitrousbox.com/' }
+  config.action_mailer.default_url_options = { host: 'http://jaxtel4219ruby-218212.use1-2.nitrousbox.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,44 @@
 require 'faker'
 
+# Create Users
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+    )
+  user.skip_confirmation!
+  user.save!
+end
+
+users = User.all
+# Note: by calling `User.new` instead of `create`,
+# we create an instance of User which isn't immediately saved to the database.
+
+# The `skip_confirmation!` method sets the `confirmed_at` attribute
+# to avoid triggering an confirmation email when the User is saved.
+
+# The `save` method then saves this User to the database.
+
+
 #Create Random Posts
 50.times do
   Post.create!(
+    user: users.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
   )
 end
 
 # Create unique post if does not already exist
-Post.where(title: "Jax Unique Seed Title", body: "Jax Unique Seed Body Data").first_or_create!
+#Post.where(user: "Jax", title: "Jax Unique Seed Title", body: "Jax Unique Seed Body Data").first_or_create!
 
 posts = Post.all
 
 #Create Comments
 100.times do 
   Comment.create!(
+    # user: users.sample,   # we have not yet associated Users with Comments
     post: posts.sample,
     body: Faker::Lorem.paragraph
   )
@@ -37,9 +60,16 @@ Question.where(title: "Jax Unique Question", body: "Jax Unique Question body tex
 
 questions = Question.all
 
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+  name: 'Jax',
+  email: 'jaxtel4219@gmail.com',
+  password: 'jack2000'
+  )
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} post created"
 puts "#{Comment.count} comments created"
 puts "#{Question.count} questions created"
-
-
