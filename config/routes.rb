d7bf6a339289dc25ec969a6e206f5e1e6679f401
jaxtel4219 <jaxtel4219@gmail.com>
2015-09-07 14:55:57 +0000

    Checkpoint-47-Destroy Assignment

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   resources :advertisements, :questions
   
   resources :topics do
-    resources :posts, except: [:index] do
-      resources :comments, only: [:create, :destroy]
-    end
+    resources :posts, except: [:index]
+  end
+
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
   end
 
     
